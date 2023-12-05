e14ea9f2b9021f3ca9dd3c07714e1cb903e3e7a7
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2023-12-05 14:54:47 +0700

    Update ClassAS.py

--- ClassAS.py
+++ ClassAS.py
@@ -7,21 +7,21 @@ class nineelevenAS:
         self.totalpriceAS= self.totalpriceAS()
     def __pricelistAS(self):
             if self.fooditemAS =='DryCuredIberianHam':
-                 self.priceAS == 177.80
+                 self.priceAS = 177.80
             elif self.fooditemAS =="Wagyusteaks":
-                self.priceAS == 450.00
+                self.priceAS = 450.00
             elif self.fooditemAS =="matsutakemushroom":
-                self.priceAS == 272.00
+                self.priceAS = 272.00
             elif self.fooditemAS =="kopiluwakcoffe":
-                self.priceAS == 306.50
+                self.priceAS = 306.50
             elif self.fooditemAS =="moosecheese":
-                self.priceAS == 487.20
+                self.priceAS = 487.20
             elif self.fooditemAS =="whitetrufles": 
-                self.priceAS == 3600.00
+                self.priceAS = 3600.00
             elif self.fooditemAS =="bluefintuna":
-                self.priceAS == 3603.00
+                self.priceAS = 3603.00
             elif self.fooditemAS=="lebonottepotatoes":
-                self.priceAS == 70.81
+                self.priceAS = 70.81
             else:
                 self.priceAS= 0.00
     def totalpriceAS(self):
