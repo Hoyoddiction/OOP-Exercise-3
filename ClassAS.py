6716d11c4d25e4789ab061d8b0f483f36ea11e5a
Hoyoddiction <144769150+Hoyoddiction@users.noreply.github.com>
2023-12-05 14:56:46 +0700

    Update ClassAS.py

--- ClassAS.py
+++ ClassAS.py
@@ -6,23 +6,23 @@ class nineelevenAS:
         self.__pricelistAS()
         self.totalpriceAS= self.totalpriceAS()
     def __pricelistAS(self):
-            if self.fooditemAS =='DryCuredIberianHam':
-                 self.priceAS = 177.80
-            elif self.fooditemAS =="Wagyusteaks":
-                self.priceAS = 450.00
-            elif self.fooditemAS =="matsutakemushroom":
-                self.priceAS = 272.00
-            elif self.fooditemAS =="kopiluwakcoffe":
-                self.priceAS = 306.50
-            elif self.fooditemAS =="moosecheese":
-                self.priceAS = 487.20
-            elif self.fooditemAS =="whitetrufles": 
-                self.priceAS = 3600.00
-            elif self.fooditemAS =="bluefintuna":
-                self.priceAS = 3603.00
-            elif self.fooditemAS=="lebonottepotatoes":
-                self.priceAS = 70.81
-            else:
-                self.priceAS= 0.00
+        if self.fooditemAS =='DryCuredIberianHam':
+                self.priceAS = 177.80
+        elif self.fooditemAS =="Wagyusteaks":
+            self.priceAS = 450.00
+        elif self.fooditemAS =="matsutakemushroom":
+            self.priceAS = 272.00
+        elif self.fooditemAS =="kopiluwakcoffe":
+            self.priceAS = 306.50
+        elif self.fooditemAS =="moosecheese":
+            self.priceAS = 487.20
+        elif self.fooditemAS =="whitetrufles": 
+            self.priceAS = 3600.00
+        elif self.fooditemAS =="bluefintuna":
+            self.priceAS = 3603.00
+        elif self.fooditemAS=="lebonottepotatoes":
+            self.priceAS = 70.81
+        else:
+            self.priceAS= 0.00
     def totalpriceAS(self):
         return self.priceAS*self.pricelistAS
